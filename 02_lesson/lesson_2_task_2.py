3571d24cea030dd30d36a071d152ba1493888965
Sprite_Spirit <Sprite.spirit.t.me@gmail.com>
2024-10-31 12:53:59 +0300

    Создана функция для проверки високосного года

---
+++ 02_lesson/lesson_2_task_2.py
@@ -0,0 +1,19 @@
+
+def is_year_leap(year):
+    """
+    Проверяет, является ли год високосным
+
+    :param year: Год для проверки
+    :return: True, если год високосный, False в противном случае
+    """
+    if year % 400 == 0:
+        return True
+    elif year % 100 == 0:
+        return False
+    elif year % 4 == 0:
+        return True
+    else:
+        return False
+
+
+print(is_year_leap(2023))
