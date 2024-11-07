aa85e9f7793be82c27f14efda4ca1051c81f5404
Sprite_Spirit <Sprite.spirit.t.me@gmail.com>
2024-11-07 11:30:15 +0300

    Внесены улучшения в документирование кода.

--- 03_lesson/user.py
+++ 03_lesson/user.py
@@ -1,16 +1,32 @@
 class User:
+    """
+    Пользователь.
+
+    Attributes:
+        first_name (str): Имя пользователя.
+        last_name (str): Фамилия пользователя.
+    """
     first_name: str
     last_name: str
 
-    def __init__(self, first_name, last_name):
-        self.first_name: str = first_name
-        self.last_name: str = last_name
+    def __init__(self, first_name: str, last_name: str) -> object:
+        self.first_name = first_name
+        self.last_name = last_name
 
-    def print_first_name(self):
+    def print_first_name(self) -> None:
+        """
+        Выводит имя пользователя.
+        """
         print(f"First name: {self.first_name}")
 
-    def print_last_name(self):
+    def print_last_name(self) -> None:
+        """
+        Выводит фамилию пользователя.
+        """
         print(f"Last name: {self.last_name}")
 
-    def print_full_name(self):
+    def print_full_name(self) -> None:
+        """
+        Выводит полное имя пользователя.
+        """
         print(f"Full name: {self.first_name}")
