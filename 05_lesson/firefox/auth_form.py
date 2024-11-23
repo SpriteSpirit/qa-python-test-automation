5d068056ebca512db878ea32fbcecfdafda4b104
SpriteSpirit <sprite.spirit.t.me@gmail.com>
2024-11-23 23:57:39 +0300

    Практика: Форма авторизации

---
+++ 05_lesson/firefox/auth_form.py
@@ -0,0 +1,52 @@
+from time import sleep
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+
+# Настройка драйвера для Firefox
+driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+
+try:
+    # Шаг 1: Открыть страницу
+    driver.get("http://the-internet.herokuapp.com/login")
+
+    # Найти поле username
+    username_field = WebDriverWait(driver, 10).until(
+        ec.element_to_be_clickable((By.ID, "username"))
+    )
+
+    # Шаг 2: Ввести имя пользователя
+    username_field.send_keys("tomsmith")
+
+    # Найти поле password
+    password_field = WebDriverWait(driver, 10).until(
+        ec.element_to_be_clickable((By.ID, "password"))
+    )
+
+    # Ожидание
+    sleep(1)
+
+    # Шаг 3: Ввести пароль
+    password_field.send_keys("SuperSecretPassword!")
+
+    # Найти кнопку "Login"
+    login_button = WebDriverWait(driver, 10).until(
+        ec.element_to_be_clickable((By.XPATH, '//*[@id="login"]/button'))
+    )
+
+    # Ожидание
+    sleep(1)
+
+    # Шаг 4: Нажать на кнопку "Login"
+    login_button.click()
+
+    # Ожидание
+    sleep(3)
+
+finally:
+    # Шаг 5: Закрыть браузер
+    driver.quit()
