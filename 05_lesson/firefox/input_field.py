3fbd13aca7b3efd18e40cf94222fc50f0d8c5fb4
SpriteSpirit <sprite.spirit.t.me@gmail.com>
2024-11-24 12:16:26 +0300

    Удалены try-finally, sleep()

--- 05_lesson/firefox/input_field.py
+++ 05_lesson/firefox/input_field.py
@@ -1,5 +1,3 @@
-from time import sleep
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.service import Service as FirefoxService
@@ -10,36 +8,26 @@ from selenium.webdriver.support import expected_conditions as ec
 # Настройка драйвера для Firefox
 driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
 
-try:
-    # Шаг 1: Открыть страницу
-    driver.get("http://the-internet.herokuapp.com/inputs")
 
-    # Найти поле ввода
-    input_field = WebDriverWait(driver, 10).until(
-        ec.presence_of_element_located((By.XPATH, '//*[@id="content"]/div/div/div/input'))
-    )
+# Шаг 1: Открыть страницу
+driver.get("http://the-internet.herokuapp.com/inputs")
 
-    #  Шаг 2: Ввести текст в поле ввода 1000
-    input_field.send_keys("1000")
-    print(1_000)
+# Найти поле ввода
+input_field = WebDriverWait(driver, 10).until(
+    ec.presence_of_element_located((By.XPATH, '//*[@id="content"]/div/div/div/input'))
+)
 
-    # Ожидание 1 сек.
-    sleep(1)
+#  Шаг 2: Ввести текст в поле ввода 1000
+input_field.send_keys("1000")
+print(1_000)
 
-    #  Шаг 3: Очистить поле
-    input_field.clear()
-    print("clear()")
+#  Шаг 3: Очистить поле
+input_field.clear()
+print("clear()")
 
-    # Ожидание 1 сек.
-    sleep(1)
+#  Шаг 4: Ввести текст в поле ввода 999
+input_field.send_keys("999")
+print(999)
 
-    #  Шаг 4: Ввести текст в поле ввода 999
-    input_field.send_keys("999")
-    print(999)
-
-    # Ожидание 3 сек.
-    sleep(3)
-
-finally:
-    # Шаг 5: Закрыть браузер
-    driver.quit()
+# Шаг 5: Закрыть браузер
+driver.quit()
