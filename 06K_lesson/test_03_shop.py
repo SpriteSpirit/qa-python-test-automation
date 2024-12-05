403841a86e4c4a7e0c6929c7e2791ebb860c978f
SpriteSpirit <sprite.spirit.t.me@gmail.com>
2024-12-05 18:24:20 +0300

    Удалены лишние импорты

--- 06K_lesson/test_03_shop.py
+++ 06K_lesson/test_03_shop.py
@@ -1,10 +1,8 @@
-from time import sleep
-
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions as ec
 from webdriver_manager.chrome import ChromeDriverManager
 import pytest
 
@@ -36,7 +34,7 @@ def test_swag_labs_checkout(driver):
 
     # Проверяем, что страница загружена, получая список товаров
     inventory_list = WebDriverWait(driver, 10).until(
-        EC.presence_of_all_elements_located((By.CLASS_NAME, 'inventory_item'))
+        ec.presence_of_all_elements_located((By.CLASS_NAME, 'inventory_item'))
     )
     assert inventory_list, "Не удалось загрузить список товаров"
 
@@ -65,7 +63,7 @@ def test_swag_labs_checkout(driver):
         'zip-code': '190000',
     }
     WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, 'first-name'))).send_keys(user_data['first-name'])
+        ec.presence_of_element_located((By.ID, 'first-name'))).send_keys(user_data['first-name'])
 
     driver.find_element(By.ID, 'last-name').send_keys(user_data['last-name'])
     driver.find_element(By.ID, 'postal-code').send_keys(user_data['zip-code'])
