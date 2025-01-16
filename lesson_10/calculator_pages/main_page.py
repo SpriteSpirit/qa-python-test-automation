d297c453dd10e7bf78d5a11f81a4b9d2e4e749a5
SpriteSpirit <sprite.spirit.t.me@gmail.com>
2025-01-17 00:12:56 +0300

    init

---
+++ lesson_10/calculator_pages/main_page.py
@@ -0,0 +1,64 @@
+from typing import Tuple
+import allure
+
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+
+from lesson_07.calculator_pages.calculator_page_enum_selectors import CalculatorSelector
+# from utilities.logger_utils import logger
+
+
+class CalculatorPage:
+    """
+    Page Object для страницы Медленный калькулятор.
+    """
+
+    def __init__(self, driver):
+        self.url = "https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html"
+        self.driver = driver
+
+    @allure.step("Открытие страницы")
+    def open(self):
+        """
+        Открыть страницу
+        """
+
+        self.driver.get(self.url)
+
+    @allure.step("Ожидание загрузки результата и проверки содержания текста")
+    def wait_to_load_result_and_compare_text(self, locator: Tuple[str, str], text: str, timeout=10):
+        """
+        Ожидание загрузки результата и проверки содержания текста
+        """
+
+        WebDriverWait(self.driver, timeout).until(ec.text_to_be_present_in_element(locator, text))
+
+    @allure.step("Клик на кнопку калькулятора")
+    def click_button(self, key: str) -> bool:
+        """
+        Клик на кнопку калькулятора
+        """
+
+        all_buttons = self.driver.find_elements(*CalculatorSelector.ANY_BUTTON.value)
+        # logger.debug(f"All buttons: {len(all_buttons)}")
+
+        for button in all_buttons:
+            # logger.debug(f"Button: {button.text}")
+
+            if button.text == key:
+                button.click()
+
+                return True
+        else:
+            return False
+
+    @allure.step("Установка задержки выполнения операции калькулятора (время в с.)")
+    def set_delay(self, delay_time: int) -> None:
+        """
+        Устанавливает задержку выполнения операций калькулятора на delay_time секунд
+        """
+
+        delay_input_field = self.driver.find_element(*CalculatorSelector.INPUT_DELAY.value)
+
+        delay_input_field.clear()
+        delay_input_field.send_keys(delay_time)
---
+++ lesson_10/tests/test_calculator_page.py
@@ -0,0 +1,61 @@
+import allure
+from utilities.logger_utils import logger
+from lesson_07.calculator_pages.main_page import CalculatorPage
+from lesson_07.calculator_pages.calculator_page_enum_selectors import CalculatorSelector
+
+
+@allure.title("Тестирование заполнения и отправки формы калькулятора")
+@allure.label("owner", "khalueva.angelina")
+@allure.feature("Заполнение и отправка формы")
+@allure.description("Проверяет результат сложения двух операндов на калькуляторе через 45 с.")
+@allure.severity(allure.severity_level.NORMAL)
+def test_form_page(chrome_driver):
+    """
+    Тестирование заполнения и отправки формы калькулятора.
+    """
+
+    delay_time = 45
+    timeout = delay_time + 5
+    expected_result = 15
+    operation_list = ['7', '+', '8', '=']
+
+    with allure.step("Создание экземпляра CalculatorPage"):
+        logger.debug("Создание экземпляра CalculatorPage")
+        calculator_page = CalculatorPage(chrome_driver)
+
+    with allure.step("Открытие страницы калькулятора"):
+        logger.debug("Открытие страницы калькулятора")
+        calculator_page.open()
+
+    with allure.step("Проверка, что страница калькулятора открылась правильно"):
+        current_url = chrome_driver.current_url
+        expected_url = calculator_page.url
+
+        assert current_url == expected_url, (f"Открыта неверная страница. Ожидалось: {expected_url}, "
+                                             f"фактически: {current_url}")
+
+    with allure.step(f"Установить задержку выполнения операций калькулятора на {delay_time} с."):
+        logger.debug(f"Установить задержку выполнения операций калькулятора на {delay_time} с.")
+
+        assert delay_time >= 0, "Ожидание должно быть положительным значением"
+
+        calculator_page.set_delay(delay_time=delay_time)
+
+    with allure.step("Выполнение операции сложения"):
+        logger.debug("Выполнение операции сложения")
+
+        assert operation_list, "Список операций пуст"
+
+        for operand in operation_list:
+            success = calculator_page.click_button(operand)
+
+            assert success, f"Не удалось нажать кнопку '{operand}'"
+
+    with allure.step(f"Ожидание {delay_time} с. и проверка результата вычислений"):
+        logger.debug(f"Ожидание {delay_time} с. и проверка результата вычислений")
+
+        try:
+            calculator_page.wait_to_load_result_and_compare_text(CalculatorSelector.RESULT_SCREEN.value,
+                                                                 text=str(expected_result), timeout=timeout)
+        except Exception as e:
+            assert False, f"Не удалось получить результат '{expected_result}'. Ошибка: {str(e)}"
